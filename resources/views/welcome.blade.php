bd8c52d870b1f2333553b55d2d9c17454feebdea
tmbb-japan <woo787@naver.com>
2023-06-28 13:00:22 +0900

    style : rotation animation added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
     <title>sailing day</title>
   </head>
   <body>
-    
     <div class="stack-container">
       <div class="album-cards">
         @foreach ($albums as $key => $album)
@@ -19,26 +18,6 @@
         @endforeach
       </div>
     </div>
-
-    <script>
-      const albumCards = document.querySelectorAll('.album-card');
-      const albumContainer = document.querySelector('.album-cards');
-      let currentIndex = 0;
-
-      function shuffleCards() {
-        albumContainer.style.transform = `translateX(-${currentIndex * 35}px)`;
-
-        albumCards.forEach((card, index) => {
-          card.classList.remove('active');
-        });
-
-        albumCards[currentIndex].classList.add('active');
-        currentIndex = (currentIndex + 1) % albumCards.length;
-      }
-
-      setInterval(shuffleCards, 500);
-    </script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
