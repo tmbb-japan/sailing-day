57d66c4219b2e5767cbad523fedb01dc53b5348c
tmbb-japan <woo787@naver.com>
2023-06-28 11:19:30 +0900

    style : carousel animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,36 @@
     <title>sailing day</title>
   </head>
   <body>
-    <div class="container">
-      <div class="carousel">
-        <figure>
-          @foreach ($albums as $album)
+    
+    <div class="stack-container">
+      <div class="album-cards">
+        @foreach ($albums as $key => $album)
+          <div class="album-card">
             <img src="{{ asset($album->image_path) }}" alt="Album Image">
-          @endforeach
-        </figure>
-      </div>  
+          </div>
+        @endforeach
+      </div>
     </div>
+
+    <script>
+      const albumCards = document.querySelectorAll('.album-card');
+      const albumContainer = document.querySelector('.album-cards');
+      let currentIndex = 0;
+
+      function shuffleCards() {
+        albumContainer.style.transform = `translateX(-${currentIndex * 35}px)`;
+
+        albumCards.forEach((card, index) => {
+          card.classList.remove('active');
+        });
+
+        albumCards[currentIndex].classList.add('active');
+        currentIndex = (currentIndex + 1) % albumCards.length;
+      }
+
+      setInterval(shuffleCards, 500);
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
