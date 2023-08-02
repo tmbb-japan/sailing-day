e1e5b95a861df8ed15bb4179fe12028c82390f1a
tmbb-japan <woo787@naver.com>
2023-08-02 12:46:07 +0900

    feat : Dynamically check the number of albums, adding CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,28 @@
   </head>
   <body>
     <div class="stack-container">
-      <div class="album-cards">
-        @foreach ($albums as $key => $album)
-          <div class="album-card">
-            <img src="{{ asset($album->image_path) }}" alt="Album Image">
-          </div>
-        @endforeach
-      </div>
+      <div class="album-cards"></div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function() {
+        const albumContainer = document.querySelector(".album-cards");
+        const albumImages = {!! json_encode($albums->pluck('image_path')) !!};
+        const numAlbums = albumImages.length;
+        const translateZ = "400px";
+
+        albumImages.forEach((imagePath, index) => {
+          const angle = (360 / numAlbums) * index;
+          const albumCard = document.createElement("div");
+          albumCard.classList.add("album-card");
+          const img = document.createElement("img");
+          img.src = "{{ asset('') }}" + imagePath;
+          img.alt = "Album Image";
+          albumCard.appendChild(img);
+          albumCard.style.transform = `translateX(-50%) translateY(-50%) rotateY(${angle}deg) translateZ(${translateZ})`;
+          albumContainer.appendChild(albumCard);
+        });
+      });
+    </script>
   </body>
 </html>
