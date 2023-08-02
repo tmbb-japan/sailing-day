dfcc706ec5f2b69be38c632a74286340d93172c9
tmbb-japan <woo787@naver.com>
2023-08-02 16:26:18 +0900

    File Organize

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,8 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
+import Album from './components/Album.vue';
 app.component('example-component', ExampleComponent);
-
+app.component('album-component', Album);
 
 app.mount('#app');
---
+++ resources/js/components/Album.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="stack-container">
+      <div class="album-cards"></div>
+    </div>
+</template>
+<script>
+    export default {
+        mounted() {
+            const albumContainer = document.querySelector(".album-cards");
+            const albumImages = {!! json_encode($albums->pluck('image_path')) !!};
+            const numAlbums = albumImages.length;
+            const translateZ = "400px";
+            albumImages.forEach((imagePath, index) => {
+                const angle = (360 / numAlbums) * index;
+                const albumCard = document.createElement("div");
+                albumCard.classList.add("album-card");
+                const img = document.createElement("img");
+                img.src = "{{ asset('') }}" + imagePath;
+                img.alt = "Album Image";
+                albumCard.appendChild(img);
+                albumCard.style.transform = `translateX(-50%) translateY(-50%) rotateY(${angle}deg) translateZ(${translateZ})`;
+                albumContainer.appendChild(albumCard);
+            });
+        }
+    }
+</script>
