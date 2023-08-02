e026640291de656b05ac0de9731465aa6d8c4ce1
tmbb-japan <woo787@naver.com>
2023-08-02 16:09:25 +0900

    scaffolding vueJs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    @vite('resources/sass/styles.scss')
+    @vite(['resources/sass/styles.scss','resources/js/app.js'])
     <title>sailing day</title>
   </head>
   <body>
     <div class="stack-container">
       <div class="album-cards"></div>
+    </div>
+    <div id="app">
+      <example-component></example-component>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
