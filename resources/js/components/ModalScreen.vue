1ce82b1ad2328232c7e4dc0f5a0fd620a346e6b2
tmbb-japan <woo787@naver.com>
2023-08-19 00:17:46 +0900

    something wron on git

--- resources/js/components/ModalScreen.vue
+++ resources/js/components/ModalScreen.vue
@@ -1,16 +1,12 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <h1>Album title</h1>
+    <div>
+        <img src="" alt="">
+        <p>Artist name</p>
+        <p>the time of released</p>
+    </div>
+    <div>
+        
     </div>
 </template>
 
